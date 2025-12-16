c3304e75570a15ed1e87280c82e07b78d6e2396a
Hrushiingole <142684900+Hrushiingole@users.noreply.github.com>
2025-12-16 17:02:07 +0530

    created all the required models.

---
+++ src/main/java/com/url/shortener/models/URLMapping.java
@@ -0,0 +1,28 @@
+package com.url.shortener.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+public class URLMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String originalUrl;
+    private String shortUrl;
+    private int clickCount=0;
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clickEventList;
+
+}
